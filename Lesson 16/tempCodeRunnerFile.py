9fb856020aab7b065e08a1bb18b7c5e72006a809
ValeriaMirza <valeriamirza5@gmail.com>
2024-05-26 22:45:05 +0300

    Add Homework 16

---
+++ Lesson 16/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+Creează un generator numit `task39` care generează toate anagramele unui șir dat ca input.
+Exemplu:
+pentru input-ul "abc"
+output-ul va fi: "abc", "acb", "bac", "bca", "cab", "cba"
+"""
